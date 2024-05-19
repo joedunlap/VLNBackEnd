87ce78abb46e0908e01f2a831a59f2f1da43906d
joedunlap <joedunlap26@gmail.com>
2024-05-19 16:11:28 -0500

    added logic so that when you get projects or a project you can see samples associated with the project

--- models/project.model.js
+++ models/project.model.js
@@ -1,70 +1,66 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
-/* eslint-disable import/extensions */
 import { db } from '../lib/database.js';
 import Constants from '../lib/constants.js';
 
 export default class ProjectsModel {
-  static getProjects = async () => {
+  static async getProjects() {
     console.log('\t\t Model : getProjects()');
+    const projects = await db.dbProjects().find({}, { projection: Constants.DEFAULT_PROJECTION }).toArray();
 
-    return db.dbProjects().find(
-      {},
-      { projection: Constants.DEFAULT_PROJECTION },
-    ).toArray();
-  };
+    // Use Promise.all to fetch samples for each project concurrently
+    const projectsWithSamples = await Promise.all(projects.map(async (project) => {
+      const samples = await db.dbSamples().find({ projectId: project.id }, { projection: Constants.DEFAULT_PROJECTION }).toArray();
+      return { ...project, samples };
+    }));
 
-  static createProject = async (newProject) => {
+    return projectsWithSamples;
+  }
+
+  static async createProject(newProject) {
     console.log('\t\t Model : createProject()');
     await db.dbProjects().insertOne(newProject);
-
     const returnProject = { ...newProject };
-    // eslint-disable-next-line no-underscore-dangle
-    delete returnProject._id;
+    delete returnProject._id; // eslint-disable-next-line no-underscore-dangle
     return returnProject;
-  };
+  }
 
-  static getProject = (id) => {
+  static async getProject(id) {
     console.log('\t\t Model : getProject()');
-    return db.dbProjects().findOne({ id }, { projection: Constants.DEFAULT_PROJECTION });
-  };
+    const project = await db.dbProjects().findOne({ id }, { projection: Constants.DEFAULT_PROJECTION });
+    if (project) {
+      const samples = await db.dbSamples().find({ projectId: id }, { projection: Constants.DEFAULT_PROJECTION }).toArray();
+      project.samples = samples;
+    }
+    return project;
+  }
 
-  static deleteProject = (id) => {
+  static async deleteProject(id) {
     console.log('\t\t Model : deleteProject()');
-
     return db.dbProjects().deleteOne({ id });
-  };
+  }
 
-  static replaceProject = async (id, project) => {
+  static async replaceProject(id, project) {
+    console.log('\t\t Model : replaceProject()');
     const result = await db.dbProjects().replaceOne({ id }, project);
-
     if (result.matchedCount === 1) {
       return project;
     }
-
     return false;
-  };
+  }
 
-  static updateProject = async (id, project) => {
-    const update = {
-      $set: {},
-    };
-
+  static async updateProject(id, project) {
+    console.log('\t\t Model : updateProject()');
+    const update = { $set: {} };
     Object.keys(project).forEach((key) => {
-      if (key === 'id') {
-        return;
-      }
-
+      if (key === 'id') return;
       update.$set[key] = project[key];
     });
-
     const result = await db.dbProjects().findOneAndUpdate({ id }, update, { returnDocument: 'after' });
-
-    if (result) {
-      // eslint-disable-next-line no-underscore-dangle
-      delete result._id;
-      return result;
+    if (result.value) {
+      delete result.value._id; // eslint-disable-next-line no-underscore-dangle
+      return result.value;
     }
-
     return false;
-  };
+  }
 }
