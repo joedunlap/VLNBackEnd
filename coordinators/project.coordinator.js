e1f44006d78d3f957d854f84068967df6e14bf80
joedunlap <joedunlap26@gmail.com>
2024-05-19 12:53:13 -0500

    worked on coordinator/schemas somem more

--- coordinators/project.coordinator.js
+++ coordinators/project.coordinator.js
@@ -1,81 +1,74 @@
 import { customAlphabet } from 'nanoid'
 import Ajv from 'ajv';
 import addFormats from 'ajv-formats';
-
+import ProjectsModel from '../models/project.model';
+import ProjectSchema from '../schemas/project.json' assert { type: 'json' };
 
 const nanoid = customAlphabet('1234567890abcdef', 10)
 
 const ajv = new Ajv();
 addFormats(ajv);
-const validate = ajv.compile(widgetSchema);
+const validate = ajv.compile(ProjectSchemaSchema);
 
 
 export default class WidgetsCoordinator {
  
-  static getProjectss = () => {
+  static getProjects = () => {
     console.log('\t Coordinator : getWidgets()');
-    return WidgetsModel.getWidgets();
+    return ProjectsModel.getProjects();
   };
 
-  /**
-   * test
-   * @param {*} newWidget 
-   * @returns newWidget
-   * @todo make this better.
-   */
-  static createWidget = (newWidget) => {
-    console.log('\t Coordinator : createWidget()');
+  static createProject = (newProject) => {
+    console.log('\t Coordinator : createProject()');
 
     const widget = {
-      ...newWidget,
-      id: uuid(),
+      ...newProject,
+      id: nanoid(),
+      createdAt: new Date()
     };
 
-    const valid = validate(widget);
+    const valid = validate(Project);
     if (!valid) {
       throw validate.errors;
     }
 
-    return WidgetsModel.createWidget(widget);
+    return ProjectsModel.createProject(project);
   };
 
-  static getWidget = (id) => {
-    console.log('\t Coordinator : getWidget()');
-    return WidgetsModel.getWidget(id);
+  static getProject = (id) => {
+    console.log('\t Coordinator : getProject()');
+    return ProjectsModel.getProject(id);
   };
 
-  static deleteWidget = (id) => {
-    console.log('\t Coordinator : deleteWidget()');
-    return WidgetsModel.deleteWidget(id);
+  static deleteProject = (id) => {
+    console.log('\t Coordinator : deleteProject()');
+    return ProjectsModel.deleteProject(id);
   };
 
-  static replaceWidget = (id, widget) => {
-    console.log('\t Coordinator : replaceWidget()');
-    const replaceWidget = {
-      ...widget,
+  static replaceProject = (id, project) => {
+    console.log('\t Coordinator : replaceProject()');
+    const replaceProject = {
+      ...project,
       id,
     };
 
-    const valid = validate(widget);
+    const valid = validate(project);
     if (!valid) {
       throw validate.errors;
     }
 
-    return WidgetsModel.replaceWidget(id, replaceWidget);
+    return ProjectsModel.replaceProject(id, replaceProject);
   };
 
-  static updateWidget = (id, widget) => {
-    console.log('\t Coordinator : updateWidget()');
+  static updateProject = (id, project) => {
+    console.log('\t Coordinator : updateProject()');
 
-    const valid = validate(widget);
+    const valid = validate(project);
     if (!valid) {
       throw validate.errors;
     }
 
-    return WidgetsModel.updateWidget(id, widget);
+    return ProjectsModel.updateProject(id, project);
   };
 
-  static addImageToWidget = (id, imagePath) => {
-    return WidgetsModel.addImageToWidget(id, imagePath);
-  };
 }
