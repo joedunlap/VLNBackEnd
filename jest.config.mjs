c7d0eb39d6f67af09bb86fd210eeebf34c2b63fa
joedunlap <joedunlap26@gmail.com>
2024-06-18 19:23:12 -0500

    cleaned up backend garbage related to testing. Will redo testing with just mocha/chai

--- jest.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-      '^.+\\.mjs$': 'babel-jest'
-    },
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node', 'mjs'],
-    extensionsToTreatAsEsm: ['.mjs'],
-  };
