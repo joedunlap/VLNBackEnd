53900bfb9240830dc0f3bb3856163530be631cd0
joedunlap <joedunlap26@gmail.com>
2024-06-20 09:23:12 -0500

    working on implementing testing. starting with projects

---
+++ test/integration/test-data/projects-data.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/prefer-default-export
+export const projectsData = [
+  {
+    id: '49b73c5f-9007-4828-a0cd-da38aacc1219',
+    name: 'Project #1',
+    briefdescription: 'test',
+    category: 'Other',
+  },
+  {
+    id: '8f00249f-66d9-4df4-ae87-e1c0b97b2e91',
+    name: 'Project #2',
+    briefdescription: 'test',
+    category: 'Other',
+  },
+  {
+    id: '0f9359f5-2ae7-46f0-acf4-d82610912f97',
+    name: 'Widget #3',
+    briefdescription: 'test',
+    category: 'Other',
+  },
+];
